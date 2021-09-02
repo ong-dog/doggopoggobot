2409cbac0e8ef6b5f10a650596e4c9762c60bbe3
ong <77343412+ong-dog@users.noreply.github.com>
2021-09-02 11:50:07 -0400

    Update doggopoggobot.py

--- doggopoggobot.py
+++ doggopoggobot.py
@@ -54,4 +54,4 @@ async def link(ctx, link:str):
 @bot.event  
 async def on_ready():
     print('online')
-bot.run('ODgyMDI1MzY2NDM1MjkxMTg2.YS1YIg.qajNSS3bVWUl_QKatYptC_EEyNY')
+bot.run('ODgyMDI1MzY2NDM1MjkxMTg2.YS1YIg.Hvy0BRTlcrxCqI5lhofq0S_Jwng')
